9a5928083b3ace02e00d34b16108783c52946bcc
progvar <prog.varadi@gmail.com>
2020-11-08 03:16:54 +0100

    style: apply customized prettier formatting

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 (function () {
-  const socket = io("http://35.157.80.184:8080");
+  const socket = io('http://35.157.80.184:8080');
 
-  socket.on("connect", console.log);
-  socket.on("message", console.log);
-  socket.on("disconnect", console.log);
+  socket.on('connect', console.log);
+  socket.on('message', console.log);
+  socket.on('disconnect', console.log);
 })();
